aefaf965c93ea7ea335e046341ff17a657785746
JungJungIn <call518@gmail.com>
2014-11-04 19:47:41 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/files/run-mininet.sh
+++ resources/puppet/files/run-mininet.sh
@@ -5,7 +5,7 @@ echo -e " ======================================================================
 echo -e "  [Install Feature] feature:install odl-dlux-core odl-restconf odl-nsf-all odl-adsal-northbound odl-mdsal-apidocs odl-l2switch-switch"
 echo -e "  [Web GUI URL] http://{ODL-IP-Address}:9191/dlux/index.html"
 echo -e " ======================================================================="
-echo
+echo -e "  Waiting.............."
 
 sleep 3
 
--- resources/puppet/files/run-openstack.sh
+++ resources/puppet/files/run-openstack.sh
@@ -5,7 +5,7 @@ echo -e " ======================================================================
 echo -e "  [Install Feature] feature:install odl-ovsdb-openstack odl-ovsdb-northbound odl-restconf odl-mdsal-apidocs odl-adsal-all odl-adsal-northbound odl-dlux-core"
 echo -e "  [Web GUI URL] http://{ODL-IP-Address}:8181/dlux/index.html"
 echo -e " ======================================================================="
-echo
+echo -e "  Waiting.............."
 
 sleep 3
 
