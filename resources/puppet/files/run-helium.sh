0ab1f24e78f3303f5edccb86f0de58215143d610
JungJungIn <call518@gmail.com>
2014-11-04 15:04:44 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/files/run-helium.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### Install Feature: OVSDB, DLUX, Etc.. ###
-# karaf> feature:install odl-ovsdb-openstack odl-ovsdb-northbound odl-restconf odl-mdsal-apidocs odl-adsal-all odl-adsal-northbound odl-dlux-core 
-
-### WEB GUI URL ###
-# http://{ODL-IP-Address}:8181/dlux/index.html
-
-echo "feature:install odl-ovsdb-openstack odl-ovsdb-northbound odl-restconf odl-mdsal-apidocs odl-adsal-all odl-adsal-northbound odl-dlux-core" | ./bin/karaf
