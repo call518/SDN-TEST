4af539d94a6388bb23988a3fe6b91cf40b496416
JungJungIn <call518@gmail.com>
2014-11-10 12:13:41 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/manifests/mininet.pp
+++ resources/puppet/manifests/mininet.pp
@@ -49,7 +49,7 @@ package { $deps:
 }
 
 exec { "Enable Backport-Precise":
-    command => "bash -c 'if [ \"`lsb_release --codename --short`\" == \"precise\" ];then cp /vagrant/resources/puppet/files/sources-precise-backport.list /etc/apt/sources.list; fi'",
+    command => "bash -c 'if [ \"`lsb_release --codename --short`\" == \"precise\" ];then cp /vagrant/resources/puppet/files/sources-precise-backport.list /etc/apt/sources.list; apt-get update; fi'",
     user    => "root",
     timeout  => "0",
     before  => Package["mininet"],
