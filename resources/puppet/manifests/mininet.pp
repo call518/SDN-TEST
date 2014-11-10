f74f987f6d23e0f76a687819dbd546bf3e9d6d8e
JungJungIn <call518@gmail.com>
2014-11-10 11:19:48 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/manifests/mininet.pp
+++ resources/puppet/manifests/mininet.pp
@@ -46,7 +46,6 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin"
 
 package { $deps:
     ensure   => installed,
-    before   => Package["mininet"],
 }
 
 exec { "Enable Backport-Precise":
