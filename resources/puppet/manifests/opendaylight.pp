448131e1723fd2155b0c63df531be216a282c8d9
JungJungIn <call518@gmail.com>
2014-11-12 17:57:24 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/manifests/opendaylight.pp
+++ resources/puppet/manifests/opendaylight.pp
@@ -89,8 +89,7 @@ exec { "Extract ODL-Helium":
 
 exec { "Patch JMX Error":
     command => "sed -i 's/0.0.0.0/127.0.0.1/g' org.apache.karaf.management.cfg",
-    creates => "/home/vagrant/opendaylight/etc",
-    cwd     => "/home/vagrant",
+    cws     => "/home/vagrant/opendaylight/etc",
     user    => "vagrant",
     timeout => "0",
     require => Exec["Extract ODL-Helium"],
