52ccbb7fb98c0a2fab5edf0988412446a32e0258
JungJungIn <call518@gmail.com>
2014-11-12 15:04:25 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/scripts/bootstrap.sh
+++ resources/puppet/scripts/bootstrap.sh
@@ -9,7 +9,7 @@ if test ! -f /etc/apt/sources.list.vagrant-bak; then
   cp -a /etc/apt/sources.list /etc/apt/sources.list.vagrant-bak
 fi
 sed -i 's/kr.archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
-sed -i 's/archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
+sed -i 's/us.archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
 sed -i 's/security.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
 sed -i 's/extras.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
 sudo apt-get update
