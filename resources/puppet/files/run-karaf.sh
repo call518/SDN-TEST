5d1cc57729ce6ab2424fd13f30e22addda5c542a
JungJungIn <call518@gmail.com>
2014-11-12 17:21:04 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/files/run-karaf.sh
+++ resources/puppet/files/run-karaf.sh
@@ -4,14 +4,14 @@ if [ "`hostname`" == "opendaylight-mininet" ]; then
 	echo
 	echo -e " ======================================================================="
 	echo -e "  [Install Feature] feature:install odl-dlux-core odl-restconf odl-nsf-all odl-adsal-northbound odl-mdsal-apidocs odl-l2switch-switch"
-	echo -e "  [Web GUI URL] http://{ODL-IP-Address}:9191/dlux/index.html"
+	echo -e "  [Web GUI URL] http://{Vagrant Host IP}:8181/dlux/index.html"
 	echo -e " ======================================================================="
 	echo -e "  Waiting.............."
 elif [ "`hostname`" == "devstack-control" ]; then
 	echo
 	echo -e " ======================================================================="
 	echo -e "  [Install Feature] feature:install odl-ovsdb-openstack odl-ovsdb-northbound odl-restconf odl-mdsal-apidocs odl-adsal-all odl-adsal-northbound odl-dlux-core"
-	echo -e "  [Web GUI URL] http://{ODL-IP-Address}:8181/dlux/index.html"
+	echo -e "  [Web GUI URL] http://{Vagrant Host IP}:8181/dlux/index.html"
 	echo -e " ======================================================================="
 	echo -e "  Waiting.............."
 fi
