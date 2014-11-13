2f8cc75772cebc332c3562a2272894647976c633
JungJungIn <call518@gmail.com>
2014-11-14 01:16:17 +0900

    update something... (run sync_gitlab.sh)

---
+++ resources/puppet/scripts/create-swap.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Exit on any error (non-zero return code)
+set -e
+
+if test ! -f /swapfile; then
+	# Create swapfile of 4GB with block size 1MB
+	/bin/dd if=/dev/zero of=/swapfile bs=1024 count=4194304
+
+	# Create swapfile of 2GB with block size 1MB
+	#/bin/dd if=/dev/zero of=/swapfile bs=1024 count=2097152
+
+	# Create swapfile of 1GB with block size 1MB
+	#/bin/dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+
+	# Set up the swap file
+	/sbin/mkswap /swapfile
+fi
+
+# Enable swap file on every boot
+if ! grep "^/swapfile"; then
+	/bin/echo '/swapfile          swap            swap    defaults        0 0' >> /etc/fstab
+fi
+
+# Enable swap file immediately
+if [ `free -m | grep :Swap | awk '{print $2}'` -eq 0 ]; then
+	/sbin/swapon /swapfile
+fi
