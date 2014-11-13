b90f52846ecfe87b86dcdc910eb5045e445730c1
JungJungIn <call518@gmail.com>
2014-11-13 15:05:05 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/manifests/devstack-control.pp
+++ resources/puppet/manifests/devstack-control.pp
@@ -15,7 +15,8 @@ vcsrepo { "/home/vagrant/devstack":
     before => File["/home/vagrant/devstack/local.conf"]
 }
 
-exec { "git checkout stable/icehouse":
+#exec { "git checkout stable/icehouse":
+exec { "git checkout stable/juno":
     cwd     => "/home/vagrant/devstack/",
     user    => "vagrant",
     timeout => "0",
