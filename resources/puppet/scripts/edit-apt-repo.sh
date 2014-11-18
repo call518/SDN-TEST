e0f6279b5061ce0903ec2ac69b0c041dffa236fd
JungJungIn <call518@gmail.com>
2014-11-18 15:57:57 +0900

    update something... (run sync_gitlab.sh)

--- resources/puppet/scripts/edit-apt-repo.sh
+++ resources/puppet/scripts/edit-apt-repo.sh
@@ -8,10 +8,11 @@ set -e
 if test ! -f /etc/apt/sources.list.vagrant-bak; then
   cp -a /etc/apt/sources.list /etc/apt/sources.list.vagrant-bak
 fi
-sed -i 's/kr.archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
-sed -i 's/us.archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
-sed -i 's/security.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
-sed -i 's/extras.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list 2> /dev/null
+sed -i 's/kr\.archive\.ubuntu\.com/ftp\.daum\.net/g' /etc/apt/sources.list 2> /dev/null
+sed -i 's/us\.archive\.ubuntu\.com/ftp\.daum\.net/g' /etc/apt/sources.list 2> /dev/null
+sed -i 's/archive\.ubuntu\.com/ftp\.daum\.net/g' /etc/apt/sources.list 2> /dev/null
+sed -i 's/security\.ubuntu\.com/ftp\.daum\.net/g' /etc/apt/sources.list 2> /dev/null
+sed -i 's/extras\.ubuntu\.com/ftp\.daum\.net/g' /etc/apt/sources.list 2> /dev/null
 
 #if sudo apt-get update; then
 #	echo "= Success 'apt-get update' ="
