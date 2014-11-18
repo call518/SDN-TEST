8f73a71877787db7c9161c43ad28784b373cf10a
JungJungIn <call518@gmail.com>
2014-11-18 16:12:48 +0900

    (Autosync: minor update...)

--- sync_gitlab.sh
+++ sync_gitlab.sh
@@ -2,5 +2,5 @@
 
 for i in `git status | grep 'deleted:' | awk '{print $NF}'`; do echo "=> git rm $i"; git rm $i; done
 git add *
-git commit -m "update something... (run sync_gitlab.sh)"
+git commit -m "(Autosync: minor update...)"
 git push
