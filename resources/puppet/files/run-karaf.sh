f46dbc26a6a9dc7690afad043175aa30b1a096d1
JungJungIn <call518@gmail.com>
2014-11-19 13:22:00 +0900

    (Autosync: minor update...)

--- resources/puppet/files/run-karaf.sh
+++ resources/puppet/files/run-karaf.sh
@@ -3,15 +3,15 @@
 if [ `hostname | grep -c "opendaylight-mininet"` -ne 0 ]; then
 	echo
 	echo -e " ======================================================================="
-	echo -e "  [e.g.) for OVS] feature:install odl-dlux-core odl-restconf odl-nsf-all odl-adsal-northbound odl-mdsal-apidocs odl-l2switch-switch"
-	echo -e "  [e.g.) for VTN] feature:install odl-adsal-compatibility-all odl-openflowplugin-all odl-vtn-manager-all odl-dlux-core"
+	echo -e "  [e.g.) for OVS] > feature:install odl-dlux-core odl-restconf odl-nsf-all odl-adsal-northbound odl-mdsal-apidocs odl-l2switch-switch"
+	echo -e "  [e.g.) for VTN] > feature:install odl-adsal-compatibility-all odl-openflowplugin-all odl-vtn-manager-all odl-dlux-core"
 	echo -e "  [Web GUI URL] http://{Vagrant Host IP}:8181/dlux/index.html"
 	echo -e " ======================================================================="
 	echo -e "  Waiting.............."
 elif [ "`hostname`" == "devstack-control" ]; then
 	echo
 	echo -e " ======================================================================="
-	echo -e "  [e.g.) for ML2] feature:install odl-ovsdb-openstack odl-ovsdb-northbound odl-restconf odl-mdsal-apidocs odl-adsal-all odl-adsal-northbound odl-dlux-core"
+	echo -e "  [e.g.) for ML2] > feature:install odl-ovsdb-openstack odl-ovsdb-northbound odl-restconf odl-mdsal-apidocs odl-adsal-all odl-adsal-northbound odl-dlux-core"
 	echo -e "  [Web GUI URL] http://{Vagrant Host IP}:8181/dlux/index.html"
 	echo -e " ======================================================================="
 	echo -e "  Waiting.............."
