9a6b5c50612437ff2ba243e327aa2d5d72aec870
JungJungIn <call518@gmail.com>
2014-11-22 23:56:36 +0900

    (Autosync: minor update...)

--- resources/puppet/manifests/opendaylight.pp
+++ resources/puppet/manifests/opendaylight.pp
@@ -180,6 +180,16 @@ file { "Put m2m-2.py":
     require => File["mkdir RESTconf-VTN-Tutorial-2"],
 }
 
+file { "Put multi-tree.py":
+    path    => "/home/vagrant/RESTconf-VTN-Tutorial-2/multi-tree.py",
+    ensure  => present,
+    owner   => "vagrant",
+    group   => "vagrant",
+    mode    => 0755,
+    content => template("/vagrant/resources/puppet/templates/multi-tree.erb"),
+    require => File["mkdir RESTconf-VTN-Tutorial-2"],
+}
+
 exec { "dos2unix /home/vagrant/RESTconf-VTN-Tutorial-2/m2m-*":
     cwd     => "/etc",
     user    => "root",
