dc29a40d47c1a428735dc216ed3bb0a89212184c
JungJungIn <call518@gmail.com>
2014-11-25 16:53:44 +0900

    vtn-coordinator: selectable - hydrogen/helium

--- sync_gitlab.sh
+++ sync_gitlab.sh
@@ -2,5 +2,6 @@
 
 for i in `git status | grep 'deleted:' | awk '{print $NF}'`; do echo "=> git rm $i"; git rm $i; done
 git add *
-git commit -m "(Autosync: minor update...)"
+#git commit -m "(Autosync: minor update...)"
+git commit -m "$1"
 git push
