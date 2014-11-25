b321f71c108ab5bf231b91da2b960a7ccb6dee19
JungJungIn <call518@gmail.com>
2014-11-25 16:59:00 +0900

    update sync_gitlab.sh

--- sync_gitlab.sh
+++ sync_gitlab.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$1X" == "X" ]; then
+    echo "Input Comment..... ex) $0 \"test commit\""
+    exit
+fi
+
 for i in `git status | grep 'deleted:' | awk '{print $NF}'`; do echo "=> git rm $i"; git rm $i; done
 git add *
 #git commit -m "(Autosync: minor update...)"
