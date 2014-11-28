4ac1354b56b8e9be81f3f586f7fd4b5ac65341e0
JungJungIn <call518@gmail.com>
2014-11-29 01:36:38 +0900

    add NTP to devstack

--- resources/puppet/manifests/devstack-control.pp
+++ resources/puppet/manifests/devstack-control.pp
@@ -6,6 +6,10 @@ include apt
 ### Export Env: Global %PATH for "Exec"
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin" ] }
 
+package { "ntp":
+    ensure   => installed,
+}
+
 package { "iptables":
     ensure   => installed,
 }
