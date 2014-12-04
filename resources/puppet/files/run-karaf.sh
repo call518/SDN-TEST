483e7fbbf183c1a563266238ff174d20efebe98b
JungJungIn <call518@gmail.com>
2014-12-04 10:55:44 +0900

    update cbench: add unless (build/install)

--- resources/puppet/files/run-karaf.sh
+++ resources/puppet/files/run-karaf.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-if [ `hostname | grep -c "opendaylight-mininet-1"` -ne 0 ]; then
+if [ `hostname | egrep -c "opendaylight-mininet-1|opendaylight-mininet-1"` -ne 0 ]; then
 	echo
 	echo -e " ======================================================================="
 	echo -e "  [e.g.) for OVS] > feature:install odl-dlux-core odl-restconf odl-nsf-all odl-adsal-northbound odl-mdsal-apidocs odl-l2switch-switch"
 	echo -e "  [e.g.) for VTN] > feature:install odl-adsal-compatibility-all odl-openflowplugin-all odl-vtn-manager-all odl-dlux-core"
+	echo -e "  [e.g.) for CBench] > feature:install odl-openflowplugin-flow-services odl-openflowplugin-drop-test"
 	echo -e "  [Web GUI URL] http://{Vagrant Host IP}:8181/dlux/index.html"
-	echo -e " ======================================================================="
-	echo -e "  Waiting.............."
-elif [ `hostname | grep -c "opendaylight-mininet-2"` -ne 0 ]; then
-	echo
-	echo -e " ======================================================================="
-	echo -e "  [e.g.) for OVS] > feature:install odl-dlux-core odl-restconf odl-nsf-all odl-adsal-northbound odl-mdsal-apidocs odl-l2switch-switch"
-	echo -e "  [e.g.) for VTN] > feature:install odl-adsal-compatibility-all odl-openflowplugin-all odl-vtn-manager-all odl-dlux-core"
-	echo -e "  [Web GUI URL] http://{Vagrant Host IP}:8282/dlux/index.html"
 	echo -e " ======================================================================="
 	echo -e "  Waiting.............."
 elif [ "`hostname`" == "devstack-control" ]; then
