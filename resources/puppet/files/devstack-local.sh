cae382d49e2b5d1227fae68d144b05fbd6fc0377
JungJungIn <call518@gmail.com>
2014-12-05 17:46:25 +0900

    update devstack

--- resources/puppet/files/devstack-local.sh
+++ resources/puppet/files/devstack-local.sh
@@ -3,6 +3,10 @@
 sudo ovs-vsctl add-port br-ex eth3
 sudo virsh net-destroy default
 sudo virsh net-undefine default
-#keystone tenant-delete demo
+neutron router-interface-delete router1 `neutron net-show private | awk '{if (NR == 13) {print $4}}'`
+neutron net-delete private
+neutron router-delete router1
+sudo ip netns del `sudo ip netns | grep "^qrouter"`
+keystone tenant-delete demo
 
 exit 0
